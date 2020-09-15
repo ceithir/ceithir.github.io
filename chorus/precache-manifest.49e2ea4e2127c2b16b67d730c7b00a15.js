f77d2282b44c649c635e5bf998b238d3eb42e5ff
Skarn <ceathramh@gmail.com>
2020-09-15 14:54:22 +0200

    Chorus: First (demo) release

---
+++ chorus/precache-manifest.49e2ea4e2127c2b16b67d730c7b00a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6c2762130e0b523d78136281adc69d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73e58307eeb3431e013a",
+    "url": "./static/css/main.7477b508.chunk.css"
+  },
+  {
+    "revision": "9beaca7f1e291b43e5d1",
+    "url": "./static/js/2.b2cedae6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.b2cedae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e58307eeb3431e013a",
+    "url": "./static/js/main.5dad2cdf.chunk.js"
+  },
+  {
+    "revision": "6e191f2f188d51a731e6",
+    "url": "./static/js/runtime-main.58e86499.js"
+  }
+]);
